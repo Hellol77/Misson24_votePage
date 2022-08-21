947a44ddfbdef2ae72f56005a024e2c66520a3f9
wJJin <wjin9707@hufs.ac.kr>
2022-08-22 04:13:22 +0900

    Feat: Backend Django logic

--- Mission24_votePage/users/views.py
+++ Mission24_votePage/users/views.py
@@ -1,3 +1,86 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import LoginForm, PostForm
+from .models import PostResult
+from django.contrib.auth import login, logout, authenticate
+from django.views.generic import FormView
 
-# Create your views here.
+class LoginView(FormView):
+    template_name = 'users/dumi_login.html'
+    form_class = LoginForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        user_id = form.cleaned_data.get("user_id")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=user_id, password=password)
+        
+        if user is not None:
+            self.request.session['user_id'] = user_id
+            login(self.request, user)
+
+        return super().form_valid(form)
+
+def main_view(request):
+    return render(request, 'users/dumi_main.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
+def PostUpload(request):
+    if request.method == 'POST':
+        if PostResult.objects.filter(user_id=request.user).exists():
+            PostResult.objects.filter(user_id=request.user)[0].delete()
+
+        team_name = request.POST['team_name']
+        team_members = request.POST['team_members']
+        intro_text = request.POST['intro_text']
+        
+        image1, image2, image3, image4 = None, None, None, None
+        image_dict_values = [image1, image2, image3, image4]
+
+        if len(request.FILES) != 0:
+            image_dict_keys = list(request.FILES.keys())
+            for i in range(len(request.FILES)):
+                image_dict_values[i] = request.FILES[image_dict_keys[i]]
+
+        postupload = PostResult(
+            user_id = request.user,
+            team_name=team_name,
+            team_members=team_members,
+            intro_text=intro_text,
+            image1=image_dict_values[0],
+            image2=image_dict_values[1],
+            image3=image_dict_values[2],
+            image4=image_dict_values[3],
+        )
+        postupload.save()
+        return redirect('/main')
+    else:
+        if PostResult.objects.filter(user_id=request.user).exists():
+            exContext = PostResult.objects.filter(user_id=request.user)[0]
+            postuploadForm = PostForm(initial={"team_name":exContext.team_name, "team_members":exContext.team_members, "intro_text":exContext.intro_text})
+        else:
+            postuploadForm = PostForm
+
+        context = {
+            'postuploadForm': postuploadForm,
+        }
+
+        return render(request, 'users/dumi_register2.html', context)
+
+def peerGroup_view(request):
+    listall = PostResult.objects.all()
+    context = {
+        'listall': listall,
+        'num': len(listall)
+    }
+    return render(request, 'users/dumi_assess.html', context)
+
+def assessDetail_view(request, pk):
+    assess = get_object_or_404(PostResult, pk=pk)
+    context = {
+        'assess': assess,
+    }
+    return render(request, 'users/dumi_assess_detail.html', context)
+ 
