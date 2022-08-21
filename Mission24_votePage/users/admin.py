570aaad180bdc15ea808769b75a9676067f3d927
wJJin <wjin9707@hufs.ac.kr>
2022-08-21 17:57:21 +0900

    Feat: Usr Setting

--- Mission24_votePage/users/admin.py
+++ Mission24_votePage/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.models import Group
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'user_id', 
+        )
+    search_fields = ('user_id',)
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
